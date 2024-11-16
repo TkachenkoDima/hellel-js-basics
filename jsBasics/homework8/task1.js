fad5f6d1a0216f0a19c7800cc4b8ece19eca00b2
Dima Tkachenko <Demitry.Tkachenko@gmail.com>
2024-11-16 14:33:12 +0200

    Added homework 8

---
+++ jsBasics/homework8/task1.js
@@ -0,0 +1,25 @@
+// Створіть масив чисел за допомогою конструкції const numbers = [2, -5, 0, 7, -3, 0, 10, -8].
+// Задайте змінні positiveCount, negativeCount та zeroCount зі значенням 0. Вони будуть використовуватись для відстеження кількості позитивних, негативних та нульових чисел в масиві.
+// Використовуючи цикл for, пройдіться по кожному елементу масиву numbers.
+// Перевірте чи число позитивне, негативне чи нульове, та збільште відповідну змінну (positiveCount, negativeCount або zeroCount) на 1.
+// Після завершення циклу виведіть значення змінних positiveCount, negativeCount та zeroCount на консоль.
+
+
+let numbers = [2, -5, 0, 7, -3, 0, 10, -8];
+let positiveCount = 0;
+let negativeCount = 0;
+let zeroCount = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        positiveCount++;
+    } else if (numbers[i] < 0) {
+        negativeCount++;
+    } else {
+        zeroCount++;
+    }
+}
+
+console.log(`Positive numbers: ${positiveCount}`);
+console.log(`Negative numbers: ${negativeCount}`);
+console.log(`Zero numbers: ${zeroCount}`)
