e40f1f19893d789821d1edbd664631119649857f
Dima Tkachenko <Demitry.Tkachenko@gmail.com>
2024-11-20 17:14:00 +0200

    Added homework 9

---
+++ jsBasics/homework9/task2.js
@@ -0,0 +1,18 @@
+// Створіть об'єкт book з такими властивостями:
+// title: рядок, представляє назву книги.
+// author: рядок, представляє автора книги.
+// year: число, представляє рік видання книги.
+// Деструктуризуйте назву та автора книги з об'єкта book.
+// Виведіть деструктуризовані змінні в консоль
+
+const book = {
+    title: 'The Gunslinger',
+    author: 'Stephen King',
+    year: 1982,
+}
+
+const {title, author} = book;
+
+let myBook = {title, author};
+
+console.log(myBook);
---
+++ jsBasics/homework9/task4.js
@@ -0,0 +1,19 @@
+// Створіть об'єкт person з такими властивостями:
+// firstName: рядок, представляє ім'я особи.
+// lastName: рядок, представляє прізвище особи.
+// age: число, представляє вік особи.
+// Додайте до об'єкта person властивість email, яка буде містити електронну пошту особи.
+// Видаліть властивість age з об'єкта person.
+// Виведіть оновлений об'єкт person в консоль.
+
+const person = {
+    firstName : 'Kek',
+    lastName : 'Ololoev',
+    age : 30,
+}
+
+person.email = 'kek@ololo.ev';
+
+delete person.age;
+
+console.log(person);
