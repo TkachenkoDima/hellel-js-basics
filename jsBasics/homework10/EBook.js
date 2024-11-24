320e6ad37cabefcb141dd77091221bbeb6737068
Dima Tkachenko <Demitry.Tkachenko@gmail.com>
2024-11-24 14:41:22 +0200

    Added homework 10

---
+++ jsBasics/homework10/EBook.js
@@ -0,0 +1,23 @@
+import { Book } from "./Book.js";
+
+export class EBook extends Book {
+  constructor(title, author, year, fileFormat) {
+    super(title, author, year);
+    this.setFileFormat(fileFormat);
+  }
+
+  printInfo() {
+    console.log(`Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Format: ${this.fileFormat}`);
+  }
+
+  getFormat() {
+    return this.fileFormat;
+  }
+
+  setFileFormat(fileFormat) {
+    if (typeof fileFormat !== "string" || fileFormat === null || fileFormat === "") 
+      throw new Error("Format must be a string and not empty");
+    
+    this.fileFormat = fileFormat;
+  }
+}
